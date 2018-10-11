57fb98d3bf5271d35a56cfc3f981d5d569d8c7fc
johnt1000 <jonatas.rodriguess@gmail.com>
2018-10-11 16:22:45 -0300

    Primeiras configurações

--- settings.py
+++ settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app_integrador_moodle_suap.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app_integrador_moodle_suap.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
