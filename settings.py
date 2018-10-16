de654cdcafc11fb23501fa75b62eb5f0af08993a
johnt1000 <jonatas.rodriguess@gmail.com>
2018-10-16 12:01:43 -0300

    Aplicado ege-django-theme

--- settings.py
+++ settings.py
@@ -56,8 +56,9 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'sincornizacao/templates')],
         'APP_DIRS': True,
+        
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
--- sincronizacao/views.py
+++ sincronizacao/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 
 def sincronizacao(request):
-    template = loader.get_template('sincronizacao.html')
-    response_body = template.render()
-    return HttpResponse(response_body)
+    return render(request, 'sincronizacao.html')
