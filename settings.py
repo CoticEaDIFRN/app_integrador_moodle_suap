e4c24224a018882d4f0223b2ed538a5e0ad24d63
Doginnn <diogenesemmanuel@gmail.com>
2018-11-05 11:07:22 -0300

    [UPD] Atualizando a sincronizacao.html

--- settings.py
+++ settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'sincronizacao',
     'ege_django_theme',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
