d1bf3d1e444e4d900931d18a166410046c61ecbe
johnt1000 <jonatas.rodriguess@gmail.com>
2019-02-12 16:33:08 -0300

    Ajuste de urls. #2

--- urls.py
+++ urls.py
@@ -1,22 +1,49 @@
-"""app_integrador_moodle_suap URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+The MIT License (MIT)
+
+Copyright 2015 Umbrella Tech.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 from django.urls import path, include
+from django.contrib import admin
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from ege_auth_jwt.views import jwt_logout
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('sincronizacao.urls')),
-]
+    path(
+        settings.URL_PATH_PREFIX,
+        include(
+            [
+                path('logout/', jwt_logout, name='logout'),
+                path('', include('ege_auth_jwt.urls', namespace='ege_auth_jwt')),
+                path('admin/', admin.site.urls),
+                path('', include('sincronizacao.urls')),
+            ]
+        )
+    ),
+    path('', RedirectView.as_view(url=settings.URL_PATH_PREFIX)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(path('%s__debug__/' % settings.URL_PATH_PREFIX, include(debug_toolbar.urls)))
